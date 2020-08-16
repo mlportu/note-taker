e0d852d68886a32998806c3361a78557e6444949
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-08-16 18:32:13 -0500

    load notes onto front end

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {notes} = require('./db/db.json');
 const fs = require('fs');
 const path = require('path');
+const short = require('short-uuid')
 
 const PORT = process.env.PORT || 3000
 const app = express();
@@ -12,42 +13,51 @@ app.use(express.urlencoded({extend:true}));
 app.use(express.json());
 app.use(express.static('public'));
 
-function createNewNote(body, notesArray){
+function createNewNote(body){
     console.log(body);
     //functions main code
     const note = body;
-    notesArray.push(note);
+    let currentNotes = fs.readFileSync('./db/db.json');
+        currentNotes = JSON.parse(currentNotes);
+        console.log(currentNotes);
+    currentNotes.push(note);
+    console.log(currentNotes)
     fs.writeFileSync(
         path.join(__dirname, './db/db.json'),
-        JSON.stringify({notes: notesArray}, null, 2)
+        JSON.stringify(currentNotes, null, 2)
     );
     //return finished conde to post route for response
     return note;
 }
 
 function validateNote(note){
+    console.log(note);
     if(!note.title || typeof note.title !== 'string'){
        return false;
     }
     if(!note.text || typeof note.text !== 'string'){
         return false;
     }
+    return true;
 }
 
 app.get('/api/notes', (req, res) => {
-    res.json(notes);
+    let results = fs.readFileSync('./db/db.json');
+    results = JSON.parse(results);
+    res.json(results);
 });
 
 app.post('/api/notes', (req, res) => {
     //set id based on what the next index of the array will be
-    req.body.id = notes.length.toString();
-
+    req.body.id = short.uuid();
+    console.log(req.body.id);
+    console.log(req.body);
     //if any data in req.body is incorrect, send 400 error back
     if(!validateNote(req.body)){
         res.status(400).send('The note is not properly formatted');
     } else{
         // add note to json file and notes array in this function
-        const note = createNewNote(req.body, notes);
+        const note = createNewNote(req.body);
         res.json(note); 
     }
 });
